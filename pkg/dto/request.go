3f93dae71b630ce86291051cc11f91a463db4702
roman <chernikovdsign@gmail.com>
2026-06-15 19:54:18 +0300

    update

--- pkg/dto/request.go
+++ pkg/dto/request.go
@@ -1,5 +1,10 @@
 package dto
 
+// TitlesDTO  represent required titles
+// swagger:model TitlesDTO
 type TitlesDTO struct {
+	// array of required titles
+	// required: true
+	// example: ["btc", "eth"]
 	Titles []string `json:"titles"`
 }
