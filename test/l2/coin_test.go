477d9a9292f9a5e1b8322125be5eac3ec97885f3
roman <chernikovdsign@gmail.com>
2026-06-23 15:13:30 +0300

    update

--- test/l2/coin_test.go
+++ test/l2/coin_test.go
@@ -35,6 +35,7 @@ func NewClient(t *testing.T) *TestClient {
 			Timeout: time.Second * 5,
 		},
 	}
+
 	return client
 }
 
@@ -46,22 +47,31 @@ func sendRequestToCoin(t *testing.T, aggregatedType string, titles []string) (in
 
 	client := NewClient(t)
 
-	var aggregatedPath string
+	requestPath := ratesPath
 	if aggregatedType != "" {
-		aggregatedPath = fmt.Sprintf("/%s", aggregatedType)
+		requestPath = fmt.Sprintf("%s/aggregated", ratesPath)
 	}
 
-	urlRaw, err := url.Parse(fmt.Sprintf("%s%s%s", basePath, ratesPath, aggregatedPath))
+	urlRaw, err := url.Parse(fmt.Sprintf("%s%s", basePath, requestPath))
 	require.NoError(t, err)
+
+	if aggregatedType != "" {
+		query := urlRaw.Query()
+		query.Set("aggregate", aggregatedType)
+		urlRaw.RawQuery = query.Encode()
+	}
 
 	requestDTO := dto.TitlesDTO{
 		Titles: titles,
 	}
+
 	data, err := json.Marshal(requestDTO)
 	require.NoError(t, err)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, urlRaw.String(), bytes.NewReader(data))
 	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
 	require.NoError(t, err)
@@ -73,20 +83,14 @@ func sendRequestToCoin(t *testing.T, aggregatedType string, titles []string) (in
 	return resp.StatusCode, body
 }
 
-func Test_AcutalCoinSuccess(t *testing.T) {
-	t.Parallel()
-
-	titles := []string{"btc", "eth"}
-
-	statusCode, body := sendRequestToCoin(t, "", titles)
-
-	require.Equal(t, http.StatusOK, statusCode)
-	require.NotEmpty(t, body)
+func requireCoinsResponse(t *testing.T, body []byte, titles []string) {
+	t.Helper()
 
 	var responseDTO dto.CoinsDTO
 
 	err := json.Unmarshal(body, &responseDTO)
 	require.NoError(t, err)
+
 	require.NotEmpty(t, responseDTO.Coins)
 	require.Len(t, responseDTO.Coins, len(titles))
 
@@ -98,6 +102,81 @@ func Test_AcutalCoinSuccess(t *testing.T) {
 
 	for _, coin := range responseDTO.Coins {
 		_, ok := expectedTitles[coin.Title]
-		require.Truef(t, ok, "wrong coin title: %s", coin.Title)
+		require.Truef(t, ok, "unexpected coin title: %s", coin.Title)
+
+		require.GreaterOrEqual(t, coin.Cost, float64(0))
+
+		delete(expectedTitles, coin.Title)
 	}
+
+	require.Empty(t, expectedTitles)
+}
+
+func Test_ActualCoinSuccess(t *testing.T) {
+	t.Parallel()
+
+	titles := []string{"btc", "eth"}
+
+	statusCode, body := sendRequestToCoin(t, "", titles)
+
+	require.Equal(t, http.StatusOK, statusCode)
+	require.NotEmpty(t, body)
+
+	requireCoinsResponse(t, body, titles)
+}
+
+func Test_AggregatedCoinSuccess(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name          string
+		aggregateType string
+	}{
+		{
+			name:          "avg",
+			aggregateType: "avg",
+		},
+		{
+			name:          "min",
+			aggregateType: "min",
+		},
+		{
+			name:          "max",
+			aggregateType: "max",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			titles := []string{"btc", "eth"}
+
+			statusCode, body := sendRequestToCoin(t, test.aggregateType, titles)
+
+			require.Equal(t, http.StatusOK, statusCode)
+			require.NotEmpty(t, body)
+
+			requireCoinsResponse(t, body, titles)
+		})
+	}
+}
+
+func Test_AggregatedCoinInvalidAggregate(t *testing.T) {
+	t.Parallel()
+
+	titles := []string{"btc", "eth"}
+
+	statusCode, body := sendRequestToCoin(t, "sum", titles)
+
+	require.Equal(t, http.StatusBadRequest, statusCode)
+	require.NotEmpty(t, body)
+
+	var responseDTO dto.ErrorDTO
+
+	err := json.Unmarshal(body, &responseDTO)
+	require.NoError(t, err)
+
+	require.Equal(t, http.StatusBadRequest, responseDTO.StatusCode)
+	require.NotEmpty(t, responseDTO.Message)
 }
