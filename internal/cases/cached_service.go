ab0fe3c15eaef9366e5e93549269022ef6c7dde7
roman <chernikovdsign@gmail.com>
2026-07-27 18:35:25 +0300

    added cached method GetCoins

--- internal/cases/cached_service.go
+++ internal/cases/cached_service.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"crypto_service/internal/entities"
 	"log/slog"
+	"sort"
 	"strings"
 
 	"github.com/pkg/errors"
+)
+
+const (
+	getCoinsOperation           = "get_coins"
+	getAggregatedCoinsOperation = "get_aggregated_coins"
 )
 
 type CachedService struct {
@@ -59,5 +65,57 @@ func (c *CachedService) GetAggregatedCoins(ctx context.Context, titles []string,
 }
 
 func (c *CachedService) GetCoins(ctx context.Context, titles []string) ([]*entities.Coin, error) {
-	return c.service.GetCoins(ctx, titles)
+	if len(titles) == 0 {
+		err := errors.Wrap(entities.ErrInvalidParam, "titles is empty")
+		slog.Error("get cached coins failed", "error", err)
+		return nil, err
+	}
+
+	cacheTitles := normalizeTitles(titles)
+
+	coins, err := c.cache.GetCoins(
+		ctx,
+		getCoinsOperation,
+		cacheTitles,
+	)
+	if err != nil {
+		// Redis не должен ломать основной запрос.
+		slog.Error(
+			"get coins from cache failed",
+			"error", err,
+			"titles", cacheTitles,
+		)
+	}
+	if coins != nil {
+		return coins, nil
+	}
+
+	coins, err = c.service.GetCoins(ctx, titles)
+	if err != nil {
+		return nil, err
+	}
+
+	cacheKey := strings.Join(cacheTitles, ":")
+
+	if err := c.cache.Store(
+		ctx,
+		getCoinsOperation,
+		cacheKey,
+		coins,
+	); err != nil {
+		slog.Error(
+			"store coins in cache failed",
+			"error", err,
+			"titles", cacheTitles,
+		)
+	}
+
+	return coins, nil
+}
+
+func normalizeTitles(titles []string) []string {
+	result := append([]string(nil), titles...)
+	sort.Strings(result)
+
+	return result
 }
